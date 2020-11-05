488ea7312210b2ccbb68d6560ff62d2f5cdba521
Aman Kumar <aman.kmr4@yahoo.com>
2020-11-05 23:48:26 +1100

    added seeds for role table

--- seeds.sql
+++ seeds.sql
@@ -4,5 +4,10 @@ INSERT INTO department (name)
 VALUES ('engineering');
 INSERT INTO department (name)
 VALUES ('Customer Service');
-INSERT INTO department (name)
-VALUES ('Intern');
+--for roles table
+INSERT INTO role (tittle, salary, department_id)
+VALUES ('Intern', 30000.00, 3);
+INSERT INTO role (tittle, salary, department_id)
+VALUES ('Manager', 100000.00, 1);
+INSERT INTO role (tittle, salary, department_id)
+VALUES ('Senior Network Engineer', 90000.00, 2);
