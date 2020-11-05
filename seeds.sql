2702e7ba9b6548b6d169ca94fcbf07df781a581a
Aman Kumar <aman.kmr4@yahoo.com>
2020-11-05 23:30:52 +1100

    added seeds.sql file we departments inserted

---
+++ seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ('sales');
+INSERT INTO department (name)
+VALUES ('engineering');
+INSERT INTO department (name)
+VALUES ('Customer Service');
+INSERT INTO department (name)
+VALUES ('Intern');
